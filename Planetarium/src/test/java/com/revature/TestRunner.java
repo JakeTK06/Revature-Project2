f73841601b1051a5aba2ebea2423268659eec120
JakeTK06 <jacobkurkowski06@gmail.com>
2025-01-27 10:41:10 -0500

    db converted to postgres

--- Planetarium/src/test/java/com/revature/TestRunner.java
+++ Planetarium/src/test/java/com/revature/TestRunner.java
@@ -17,7 +17,7 @@ import java.time.Duration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "classpath:features",
+        features = "classpath:features/UserRegistration.feature",
         glue = "com.revature.steps",
         plugin = {"pretty","html:src/test/resources/reports/html-report.html","json:src/test/resources/reports/json-report.json"}
 )
--- Planetarium/src/test/java/com/revature/utility/DatabaseConnector.java
+++ Planetarium/src/test/java/com/revature/utility/DatabaseConnector.java
@@ -1,6 +1,6 @@
 package com.revature.utility;
 
-import org.sqlite.SQLiteConfig;
+//import org.sqlite.SQLiteConfig;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,9 +8,12 @@ import java.sql.SQLException;
 
 public class DatabaseConnector {
     public static Connection getConnection() throws SQLException {
-        SQLiteConfig config = new SQLiteConfig();
-        config.enforceForeignKeys(true);
-        String url = System.getenv("DATABASE_URL");
-        return DriverManager.getConnection(url, config.toProperties());
+//        SQLiteConfig config = new SQLiteConfig();
+//        config.enforceForeignKeys(true);
+//        String url = System.getenv("DATABASE_URL");
+//        return DriverManager.getConnection(url, config.toProperties());
+        return DriverManager.getConnection("jdbc:postgresql://trng-2121-2.chueiwozbnfz.us-east-1.rds.amazonaws.com:5432/postgres",
+                "postgres",
+                "trng_rds_2121");
     }
 }
